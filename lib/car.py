1db64d5e8a599e96dba12e6b85908f2ded968e68
Marozva <isabokefrankline@gmail.com>
2023-11-30 13:34:54 +0300

    defining object inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,17 @@
 from vehicle import Vehicle
 
+
 class Car(Vehicle):
-    pass
+    '''A class representing a car, inheriting from Vehicle.'''
+
+    def __init__(self, wheel_size: int, wheel_number: int):
+        '''Initialize a new Car instance.'''
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        '''Simulate the car's movement.'''
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        '''Simulate filling up the car's tank.'''
+        return super().fill_up_tank()
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,15 @@
 class Vehicle:
-    pass
+    '''A class representing a generic vehicle.'''
+
+    def __init__(self, wheel_size: int, wheel_number: int):
+        '''Initialize a new Vehicle instance.'''
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        '''Simulate the vehicle's movement.'''
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        '''Simulate filling up the vehicle's tank.'''
+        return "filling up!"
